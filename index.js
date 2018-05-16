d2a8b7d9b38e9bc88e8a6323c8856399d202dd24
rickhyun <38196055+rickhyun@users.noreply.github.com>
2018-05-17 08:34:49 +1000

    Update index.js
    added command
    fixed bugs for uint64

--- index.js
+++ index.js
@@ -2,28 +2,29 @@
 * Created by rickhyun on 2018-05-14.
 */
 const Command = require('command')
-const Long = require("long");
+const Long = require("long")
 const config = require('./config.json')
 
 module.exports = function MyDPS(d) {
   const command = Command(d)
 
-  let enable = config.enable;
+  let enable = config.enable
+      notice = config.notice
 
   let gid,
-  boss = new Set();
-  enraged = false,
-  inHH = false,
-  hpCur=0,
-  hpMax=0,
-  newbossflag=0,
-  starttime=0,
-  endtime=0,
-  totaldamage= new Long(0,0);
+      boss = new Set(),
+      enraged = false,
+      inHH = false,
+      hpCur=0,
+      hpMax=0,
+      newbossflag=0,
+      starttime=0,
+      endtime=0,
+      totaldamage= new Long(0,0)
 
   d.hook('S_LOGIN', (e) => {
-    gid=e.gameId;
-  });
+    gid=e.gameId
+  })
 
   d.hook('S_LOAD_TOPO', (e) => {
     (e.zone === 9950) ? inHH = true : inHH = false
@@ -35,60 +36,92 @@ module.exports = function MyDPS(d) {
     if (!boss.has(e.id.toString())) {
       //new BAM
       boss.add(e.id.toString())
-      hpMax = e.maxHp;
-      newbossflag = 1;
-      totaldamage = 0;
-      //send('new boss : ' + e.id + ' exboss :' + bossid);
+      hpMax = e.maxHp
+      newbossflag = 1
     }
-    hpCur = e.curHp;
+    hpCur = e.curHp
     hpPer = Math.floor((hpCur / hpMax) * 100)
-    // someone hit the boss : started battle.
-    if(newbossflag == 1 && hpCur < hpMax){
-      // battle started
-      starttime = Date.now();
-      newbossflag = 0;
-    }
-  });
+  })
 
   d.hook('S_EACH_SKILL_RESULT', (e) => {
     if (!enable || inHH) return
     if(gid.equals(e.source) && e.damage > 0 && boss.has(e.target.toString())){
-      totaldamage = e.damage.add(totaldamage);
+      if(newbossflag == 1) {
+        starttime = Date.now()
+        totaldamage = e.damage
+        newbossflag = 0
+      }
+      else totaldamage = e.damage.add(totaldamage)
     }
-  });
+  })
 
   d.hook('S_DESPAWN_NPC', (e) => {
     if (!enable || inHH) return
     if (boss.has(e.gameId.toString())) {
-      printoutdps();
-      totaldamage=0;
+      printoutdps()
       boss.delete(e.gameId.toString())
     }
   })
 
   d.hook('S_NPC_STATUS', (e) => {
-      if (!enable || inHH) return
-      if (!boss.has(e.creature.toString())) return
-      if (e.enraged === 1 && !enraged) {
-          printoutdps();
-          enraged = true
-      } else if (e.enraged === 0 && enraged) {
-          if (hpPer === 100) return
-          printoutdps();
-          enraged = false
-      }
+    if (!enable || inHH) return
+    if (!boss.has(e.creature.toString())) return
+    if (e.enraged === 1 && !enraged) {
+      printoutdps()
+      enraged = true
+    } else if (e.enraged === 0 && enraged) {
+      if (hpPer === 100) return
+      printoutdps()
+      enraged = false
+    }
   })
 
   function printoutdps()
   {
-    partydamage=hpMax-hpCur;
-    if(partydamage === 0) return
-    endtime=Date.now();
-    battleduration = Math.floor((endtime-starttime) / 1000);
+    partydamage=hpMax-hpCur
+    if(partydamage === 0 || totaldamage === 0) return
+    endtime=Date.now()
+    battleduration = Math.floor((endtime-starttime) / 1000)
     if(battleduration==0) return
-    send( (totaldamage/1000/battleduration).toFixed(1) + ' k/s ' + Math.floor(totaldamage*100/(partydamage)) + '%' + ' duration : ' + battleduration.toFixed(0) + 'seconds');
+    toChat( (totaldamage.div(1000*battleduration).toFixed(1) + 'k/s '.clr('E69F00')
+            + Math.floor(totaldamage.div(partydamage)*100)  + '% '.clr('E69F00')
+            + battleduration.toFixed(0) + 'seconds'.clr('E69F00'))
   }
 
-  function send(msg) { command.message(`[MYDPS] : ` + [...arguments].join('\n\t - '.clr('FFFFFF'))) }
 
-};
+  // helper
+  function toChat(msg) {
+    if (notice) d.toClient('S_DUNGEON_EVENT_MESSAGE', {
+      unk1: 31, // 42 blue shiny text, 31 normal Text
+      unk2: 0,
+      unk3: 27,
+      message: msg
+    })
+    else send(msg)
+  }
+
+  // command
+  command.add('mydps', (arg) => {
+    // toggle
+    if (!arg) {
+      enable = !enable
+      send(`${enable ? 'Enabled'.clr('56B4E9') : 'Disabled'.clr('E69F00')}`)
+    }
+    else if (arg == 'c' || arg=='current') {
+      printoutdps()
+    }
+    // notice
+    else if (arg === 'n' || arg === 'ㅜ' || arg === 'notice') {
+      notice = !notice
+      send(`Notice to screen ${notice ? 'enabled'.clr('56B4E9') : 'disabled'.clr('E69F00')}`)
+      // status
+    } else if (arg === 's' || arg === 'ㄴ' || arg === 'status') status()
+    else send(`Invalid argument.`.clr('FF0000') + ' mydps or mydps c/n/s')
+  })
+  function send(msg) { command.message(`[mydps] : ` + [...arguments].join('\n\t - '.clr('FFFFFF'))) }
+  function status() { send(
+    `Enrage message : ${enable ? 'Enabled'.clr('56B4E9') : 'Disabled'.clr('E69F00')}`,
+    `Notice to screen : ${notice ? 'Enabled'.clr('56B4E9') : 'Disabled'.clr('E69F00')}`)
+  }
+
+}
