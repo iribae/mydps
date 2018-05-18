f1c91da8f24693e3f2556cc01f8292658cfd4200
rickhyun <38196055+rickhyun@users.noreply.github.com>
2018-05-18 12:25:53 +1000

    Update index.js
    notice msg

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ module.exports = function MyDPS(d) {
 
   let enable = config.enable
       notice = config.notice
+      notice_damage = config.notice_damage
 
-  let gid,
+  let gid=0,
       boss = new Set(),
       enraged = false,
       inHH = false,
@@ -47,10 +48,10 @@ module.exports = function MyDPS(d) {
 
   d.hook('S_EACH_SKILL_RESULT', (e) => {
     if (!enable || inHH) return
+    if(gid === 0) gid = e.source // for debug
     if(gid.equals(e.source) && e.damage > 0 && boss.has(e.target.toString())){
-    //if(e.damage > 0 && boss.has(e.target.toString())){ // for debug
       if (extarget.notEquals(e.target)){
-        if(totaldamage.gt(0)) printoutdps(expartydamage)
+        if(totaldamage.gt(0)) toChat(caldps(expartydamage))
         starttime = Date.now()
         totaldamage = e.damage
         //toChat('new')
@@ -59,13 +60,16 @@ module.exports = function MyDPS(d) {
       //toChat('totaldamage' + totaldamage.toString())
       extarget = e.target;
       expartydamage = hpMax.sub(hpCur)
+
+      if(e.damage.gt(notice_damage)) toNotice(caldps(partydamage))
+
     }
   })
 
   d.hook('S_DESPAWN_NPC', (e) => {
     if (!enable || inHH) return
     if (boss.has(e.gameId.toString())) {
-      printoutdps(partydamage)
+      toChat(caldps(partydamage))
       boss.delete(e.gameId.toString())
     }
   })
@@ -74,50 +78,71 @@ module.exports = function MyDPS(d) {
     if (!enable || inHH) return
     if (!boss.has(e.creature.toString())) return
     if (e.enraged === 1 && !enraged) {
-      printoutdps(partydamage)
+      toChat(caldps(partydamage))
       enraged = true
     } else if (e.enraged === 0 && enraged) {
       if (hpPer === 100) return
-      printoutdps(partydamage)
+      toChat(caldps(partydamage))
       enraged = false
     }
   })
 
-  function printoutdps(damage)
+  function caldps(damage)
   {
     endtime=Date.now()
-    if( damage === 0 || totaldamage === 0 || starttime === 0 || endtime <= starttime) {
+    battleduration = Math.floor((endtime-starttime) / 1000)
+    if( damage === 0 || totaldamage === 0 || starttime === 0 || endtime <= starttime || battleduration == 0 ) {
       toChat( 'totaldamage ' + totaldamage.toString() + 'battleduration ' + Math.floor((endtime-starttime) / 1000) + 'damage ' + damage.toString() );
       return
     }//debug
     //else toChat( 'totaldamage ' + totaldamage.toString() + 'battleduration ' + Math.floor((endtime-starttime) / 1000) + 'damage ' + damage.toString() );
 
-    battleduration = Math.floor((endtime-starttime) / 1000)
-    toChat( Math.floor(totaldamage.div(1000 * battleduration)) + 'k/s '.clr('E69F00')
+    dpsmsg = Math.floor(totaldamage.div(1000 * battleduration)) + 'k/s '.clr('E69F00')
             + Math.floor( totaldamage.multiply(100).div(damage))  + '% '.clr('E69F00')
-            + battleduration.toFixed(0) + 'seconds'.clr('E69F00') )
+            + battleduration.toFixed(0) + 'seconds'.clr('E69F00')
+
+    return dpsmsg
   }
 
   // helper
-  function toChat(msg) {
+  function toChat(msg) {send(msg)}
+  function toNotice(msg) {
     if (notice) d.toClient('S_DUNGEON_EVENT_MESSAGE', {
-      unk1: 31, // 42 blue shiny text, 31 normal Text
+      unk1: 41, // 42 blue shiny text, 31 normal Text candidate 38
       unk2: 0,
       unk3: 27,
       message: msg
     })
-    else send(msg)
+   }
+  function send(msg) { command.message(`[my-dps] : ` + [...arguments].join('\n\t - '.clr('FFFFFF'))) }
+  function status() { send(
+    `Enrage message : ${enable ? 'Enabled'.clr('56B4E9') : 'Disabled'.clr('E69F00')}`,
+    `Notice to screen : ${notice ? 'Enabled'.clr('56B4E9') : 'Disabled'.clr('E69F00')}`)
+  }
+  function log(msg) {
+      console.log(`[FPS-UTILS] - ${msg}`);
   }
 
   // command
-  command.add('mydps', (arg) => {
+  command.add('my-dps', (arg, arg2,arg3) => {
     // toggle
     if (!arg) {
       enable = !enable
       send(`${enable ? 'Enabled'.clr('56B4E9') : 'Disabled'.clr('E69F00')}`)
     }
     else if (arg == 'c' || arg=='current') {
-      printoutdps(partydamage)
+      caldps(partydamage)
+    }
+    else if (arg == 'h' || arg=='history') {
+      caldps(partydamage)
+    }
+    else if (arg == 't' || arg=='test') {
+      d.toClient('S_DUNGEON_EVENT_MESSAGE', {
+       unk1: arg3, // 42 blue shiny text, 31 normal Text 70 FLASHING_NOTIFICATION
+       unk2: 0,
+       unk3: 27,
+       message: arg2 + ' ' + arg3
+     })
     }
     // notice
     else if (arg === 'n' || arg === 'ㅜ' || arg === 'notice') {
@@ -125,12 +150,11 @@ module.exports = function MyDPS(d) {
       send(`Notice to screen ${notice ? 'enabled'.clr('56B4E9') : 'disabled'.clr('E69F00')}`)
       // status
     } else if (arg === 's' || arg === 'ㄴ' || arg === 'status') status()
-    else send(`Invalid argument.`.clr('FF0000') + ' mydps or mydps c/n/s')
+    else send(`Invalid argument.`.clr('FF0000') + ' dps or dps c/n/s')
   })
-  function send(msg) { command.message(`[mydps] : ` + [...arguments].join('\n\t - '.clr('FFFFFF'))) }
-  function status() { send(
-    `Enrage message : ${enable ? 'Enabled'.clr('56B4E9') : 'Disabled'.clr('E69F00')}`,
-    `Notice to screen : ${notice ? 'Enabled'.clr('56B4E9') : 'Disabled'.clr('E69F00')}`)
-  }
 
+
+  this.destructor = () => {
+		command.remove('my-dps');
+	};
 }
